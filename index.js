c12bf97428013cc4693d117530c884e4a1078a5b
Alexis Araujo <ala1289@gmail.com>
2020-03-07 23:11:01 -0600

    Instalacion CORS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 const app = express();
 const server = require('http').Server(app);
 const socket = require('./socket');
-
+const cors = require('cors');
 const { config } = require('./config/index');
 const router = require('./utils/network/routes');
 
@@ -12,6 +12,7 @@ db();
 //body parser
 app.use(express.json());
 app.use(helmet());
+app.use(cors());
 app.use('/app', express.static('public'));
 socket.connect(server);
 router(app);
